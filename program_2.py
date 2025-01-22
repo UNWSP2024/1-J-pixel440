6482f38d01704cf7256a0099d0ee3c808988ccb1
J-pixel440 <Joshplath1@gmail.com>
2025-01-22 14:06:17 -0600

    Proverbs 22:29 program upload
    Written by Josh Plath on January 21, 2025.

--- program_2.py
+++ program_2.py
@@ -1,6 +1,8 @@
-def proverbs_22_29():
-    # Write your Proverbs 22:29 code below
-    print('...')
-    
-
-proverbs_22_29()
+print()
+print('"Do you see a man skillful in his work?')
+print()
+print('He will stand before kings;')
+print()
+print('he will not stand before obscure men"')
+print()
+print('   (Proverbs 22:29)')
